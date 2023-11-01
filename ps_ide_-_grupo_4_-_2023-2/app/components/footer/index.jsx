e5d7daade520aa49f5ab1505b2d6388f04d8756d
rafafaremb <rafaelarembold@hotmail.com>
2023-11-01 17:37:43 -0300

    terminei o footer.

--- ps_ide_-_grupo_4_-_2023-2/app/components/footer/index.jsx
+++ ps_ide_-_grupo_4_-_2023-2/app/components/footer/index.jsx
@@ -3,8 +3,24 @@ export default function Footer(props){
     return(
         <>
         <div className={styles.container}>
-            <span>株式会社ポケモン (The Pokémon Company) © x IDE © Todos os direitos reservados.</span>
-            <span>Termos de Uso | Politica de Privacidade | Política de Cookies</span>
+            <imagens>
+                <a href="https://www.pokemon.com/br/">
+                    <img src='/images/pokemon-logo.png' style= {{width: '56px', height: '56px'}}/>
+                </a>
+                <a href="https://www.instagram.com/ide_jr/">
+                    <img src='/images/instagram-logo.png' style= {{width: '56px', height: '56px'}}/>
+                </a>
+                <a href="https://www.linkedin.com/company/empresa-j%C3%BAnior-ide/?originalSubdomain=br">
+                    <img src='/images/linkedin-logo.png' style= {{width: '56px', height: '56px'}}/>
+                </a>
+                <a href="https://www.idejr.com.br/">
+                    <img src='/images/logo-pixelado-branco.png' style= {{width: '100px', height: '56px'}}/>
+                </a>
+            </imagens>
+            <span>株式会社ポケモン (The Pokémon Company) © x IDE © Todos os direitos reservados. Termos de Uso | Politica de Privacidade | Política de Cookies</span>
+            <ide>
+                <img src='/images/logo-pixelado-branco.png' style= {{width: '150px', height: '80px'}}/>
+            </ide>
         </div>
         </>
     )
