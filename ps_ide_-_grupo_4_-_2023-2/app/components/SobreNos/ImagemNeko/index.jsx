765ad2144fd4c870416ad4d51531d3c1f67cf35f
GabrielWWelter <gabrielwwelter@gmail.com>
2023-11-02 12:07:11 -0300

    Sobre nós
    Boa parte da sessão concluída, com exceção de alguns ajustes de espaçamento, fonte e localização de imagens

---
+++ ps_ide_-_grupo_4_-_2023-2/app/components/SobreNos/ImagemNeko/index.jsx
@@ -0,0 +1,10 @@
+import styles from './style.module.css'
+export default function ImagemNeko(props)
+{ return(
+    <>
+        <div className={styles.container}>
+        <img src='/images/neko-comemorando.png'/>
+        </div>
+    </>
+    )
+}
--- ps_ide_-_grupo_4_-_2023-2/app/components/SobreNos/TextoPrincipal/index.jsx
+++ ps_ide_-_grupo_4_-_2023-2/app/components/SobreNos/TextoPrincipal/index.jsx
@@ -3,12 +3,22 @@ export default function TextoPrincipal(props)
 {
     return(
         <>
-        <div className={styles.container}>
-            <p> paragrafo </p>
+        <div className={styles.border}>
+            <div className={styles.container}>
+                <p> A IDE é a empresa júnior dos cursos<br></br> 
+                Ciência e Engenharia de Computação da Universidade Federal do Rio Grande do <br></br>
+                Sul. Formada por alunos interessados em <br></br>
+                se tornarem empreendedores e profissionais excepcionais, a IDE fornece <br></br>
+                soluções em software, como sites e <br></br>
+                sistemas, com preços abaixo do <br></br>
+                mercado, aliados à excelência da UFRGS, <br></br>
+                o Instituto de Informática e seus professores, que fornecem apoio técnico <br></br>
+                e aconselhamento aos estudantes.<br></br>
+                 </p>
+                <img src='/imagens/logo-pixelado-branco.png' alt="Logo" className={styles.logo}/>
+             </div>
         </div>
-        <span>
-            <img src='/imagens/logo-pixelado-branco.png'/>
-        </span>
+        
         </>
     )
 }
