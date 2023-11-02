2f177b7b2a28b06735005b5c63fd6cab660b6ff2
PLLima <{ID}+{username}@users.noreply.github.com>
2023-11-02 07:51:09 -0300

    Parcialmente implementada responsividade do footer

--- ps_ide_-_grupo_4_-_2023-2/app/components/footer/index.jsx
+++ ps_ide_-_grupo_4_-_2023-2/app/components/footer/index.jsx
@@ -5,49 +5,51 @@ export default function Footer(){
     return(
         <>
             <div className={styles.container}>
-                <imagens>
-                    <a href="https://www.pokemon.com/br/">
-                        <Image
-                            src="/images/pokemon-logo.png"
-                            height={56}
-                            width={56}
-                            alt=""
-                        />
-                    </a>
-                    <a href="https://www.instagram.com/ide_jr/">
-                        <Image
-                            src="/images/instagram-logo.png"
-                            height={56}
-                            width={56}
-                            alt=""
-                        />
-                    </a>
-                    <a href="https://www.linkedin.com/company/empresa-j%C3%BAnior-ide/?originalSubdomain=br">
-                        <Image
-                            src="/images/linkedin-logo.png"
-                            height={56}
-                            width={56}
-                            alt=""
-                        />
-                    </a>
-                    <a href="https://www.idejr.com.br/">
-                        <Image
-                            src="/images/logo-pixelado-branco.png"
-                            height={56}
-                            width={100}
-                            alt=""
-                        />
-                    </a>
-                </imagens>
-                <span>株式会社ポケモン (The Pokémon Company) © x IDE © Todos os direitos reservados. Termos de Uso | Politica de Privacidade | Política de Cookies</span>
-                <ide>
+                <a href="https://www.pokemon.com/br/">
                     <Image
-                        src="/images/logo-pixelado-branco.png"
-                        height={80}
-                        width={150}
+                        className={styles.imagens}
+                        src="/images/pokemon-logo.png"
+                        height={100}
+                        width={100}
                         alt=""
                     />
-                </ide>
+                </a>
+                <a href="https://www.instagram.com/ide_jr/">
+                    <Image
+                        className={styles.imagens}
+                        src="/images/instagram-logo.png"
+                        height={100}
+                        width={100}
+                        alt=""
+                    />
+                </a>
+                <a href="https://www.linkedin.com/company/empresa-j%C3%BAnior-ide/?originalSubdomain=br">
+                    <Image
+                    className={styles.imagens}
+                    src="/images/linkedin-logo.png"
+                    height={100}
+                    width={100}
+                    alt=""
+                    />
+                </a>
+                <a href="https://www.idejr.com.br/">
+                    <Image
+                        className={styles.imagens}
+                        src="/images/logo-pixelado-branco.png"
+                        height={400}
+                        width={750}
+                        alt=""
+                    />
+                </a>
+                <span>株式会社ポケモン (The Pokémon Company) © x IDE © Todos os direitos reservados.</span>
+                <span>Termos de Uso | Politica de Privacidade | Política de Cookies</span>
+                <Image
+                        className={styles.ide}
+                        src="/images/logo-pixelado-branco.png"
+                        height={400}
+                        width={750}
+                        alt=""
+                />
             </div>
         </>
     )
