f5a5c001f86705b84e5609c86ca4ab1b6d70c248
PLLima <{ID}+{username}@users.noreply.github.com>
2023-11-02 04:44:56 -0300

    Adicionada responsividade ao header

--- ps_ide_-_grupo_4_-_2023-2/app/components/Banner/index.jsx
+++ ps_ide_-_grupo_4_-_2023-2/app/components/Banner/index.jsx
@@ -1,11 +1,12 @@
-import styles from './style.module.css'
+import styles from './banner.module.css'
 
 export default function Banner()
 {
     return(
         <>
             <div className={styles.container}>
-
+                <h2>Teste 1</h2>
+                <h3>Teste 2</h3>
             </div>
         </>
     )
