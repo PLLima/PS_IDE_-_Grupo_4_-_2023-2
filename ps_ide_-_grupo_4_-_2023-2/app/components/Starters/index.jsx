ba69afaec077f461ae84548a9866dceee7782c60
Afonso <afonsobritto@hotmail.com>
2023-11-03 14:17:50 -0300

    coloquei os botoes

--- ps_ide_-_grupo_4_-_2023-2/app/components/Starters/index.jsx
+++ ps_ide_-_grupo_4_-_2023-2/app/components/Starters/index.jsx
@@ -2,31 +2,33 @@ import styles from './style.module.css';
 
 export default function Starters(props){
     return(
-        <>
-            <div className={styles.wrapper}>
-                <h1 className={styles.title}>Água</h1>
-                    <div className={styles.container}>
-                        <div className={styles.grid}>
-                            <img src={'images/pokemons/agua/1.png'}/>
-                            <img src={'images/pokemons/agua/2.png'}/>
-                            <img src={'images/pokemons/agua/3.png'}/>
-                            <img src={'images/pokemons/agua/4.png'}/>
-                            <img src={'images/pokemons/agua/5.png'}/>
-                            <img src={'images/pokemons/agua/6.png'}/>
-                            <img src={'images/pokemons/agua/7.png'}/>
-                            <img src={'images/pokemons/agua/8.png'}/>
-                            <img src={'images/pokemons/agua/9.png'}/>
-                            <img src={'images/pokemons/agua/10.png'}/>
-                            <img src={'images/pokemons/agua/11.png'}/>
-                            <img src={'images/pokemons/agua/12.png'}/>
-                                <div className={styles.logos}>
-                                    <img src={'images/tipos/Agua_longo.png'}/>
-                                    <img src={'images/tipos/Planta_longo.png'}/>
-                                    <img src={'images/tipos/Fogo_longo.png'}/>
-                                </div>
+        <>  
+                <div className={styles.wrapper}>
+                    <h1 className={styles.title}>ÁGUA</h1>
+                        <div className={styles.container}>
+                            <div className={styles.grid}>
+                                <img src={'images/pokemons/agua/1.png'}/>
+                                <img src={'images/pokemons/agua/2.png'}/>
+                                <img src={'images/pokemons/agua/3.png'}/>
+                                <img src={'images/pokemons/agua/4.png'}/>
+                                <img src={'images/pokemons/agua/5.png'}/>
+                                <img src={'images/pokemons/agua/6.png'}/>
+                                <img src={'images/pokemons/agua/7.png'}/>
+                                <img src={'images/pokemons/agua/8.png'}/>
+                                <img src={'images/pokemons/agua/9.png'}/>
+                                <img src={'images/pokemons/agua/10.png'}/>
+                                <img src={'images/pokemons/agua/11.png'}/>
+                                <img src={'images/pokemons/agua/12.png'}/>
+                                    
+                            </div>
                         </div>
-                    </div>
-            </div>
+                </div>
+                <div className={styles.logos}>
+                    <button> <img src={'images/tipos/Agua_longo.png'} /> </button>
+                    <button> <img src={'images/tipos/Planta_longo.png'} /> </button>
+                    <button> <img src={'images/tipos/Fogo_longo.png'} /> </button>
+                </div>
+            
         </>
     )
 }
