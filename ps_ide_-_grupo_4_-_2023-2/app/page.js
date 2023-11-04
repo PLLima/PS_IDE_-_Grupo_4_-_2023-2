bb9b1861b428f8612374f7b9f92aea8f4789fe90
rafafaremb <rafaelarembold@hotmail.com>
2023-11-03 22:08:11 -0300

    inicio starters

--- ps_ide_-_grupo_4_-_2023-2/app/page.js
+++ ps_ide_-_grupo_4_-_2023-2/app/page.js
@@ -4,13 +4,15 @@ import Forms from './components/Forms';
 import SobreNos from './components/SobreNos'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Starters from './components/Starters'
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Starters/>
       <SobreNos/>
-      <Forms><Forms/>
+      <Forms/>
       <Footer/>
     </>
   )
