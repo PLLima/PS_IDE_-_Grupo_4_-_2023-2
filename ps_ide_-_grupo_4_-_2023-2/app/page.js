be27641f333f3f4b12d4e5d860f557c3ba6d20d0
GabrielWWelter <gabrielwwelter@gmail.com>
2023-11-03 22:52:40 -0300

    .
    .

--- ps_ide_-_grupo_4_-_2023-2/app/page.js
+++ ps_ide_-_grupo_4_-_2023-2/app/page.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-import Forms from './components/Forms'
+
+import Forms from './components/Forms';
 import SobreNos from './components/SobreNos'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import StartersAgua from './components/StartersAgua'
+import StartersAgua from './components/StartersAgua';
 
 export default function Home() {
   return (
