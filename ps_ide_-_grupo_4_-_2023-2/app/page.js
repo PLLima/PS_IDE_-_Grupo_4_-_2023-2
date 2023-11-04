c89bcb9bf54a41e4255e8a731021ecc2013ff192
GabrielWWelter <gabrielwwelter@gmail.com>
2023-11-04 00:30:46 -0300

    União
    Tudo que não era compatível eu uni em uma única branch

--- ps_ide_-_grupo_4_-_2023-2/app/page.js
+++ ps_ide_-_grupo_4_-_2023-2/app/page.js
@@ -4,12 +4,14 @@ import SobreNos from './components/SobreNos'
 import Forms from './components/Forms'
 import Footer from './components/Footer'
 import StartersAgua from './components/StartersAgua'
+import StarterMain from "./components/StartersMain"
 
 export default function Home(){
   return(
     <>
       <Header/>
       <Banner/>
+      <StarterMain/>
       <StartersAgua/>
       <SobreNos/>
       <Forms/>
