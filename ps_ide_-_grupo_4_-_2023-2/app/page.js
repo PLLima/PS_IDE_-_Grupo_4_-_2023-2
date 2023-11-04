d11b69af0f5067f011442a52375e597a5027c828
GabrielWWelter <gabrielwwelter@gmail.com>
2023-11-03 21:00:54 -0300

    Template da água
    Quase pronto o template da água, algumas coisas a se mudar, como o fundo, por exemplo, em breve vai da pra usar como modelo para os outros mudando apenas as cores e os textos

--- ps_ide_-_grupo_4_-_2023-2/app/page.js
+++ ps_ide_-_grupo_4_-_2023-2/app/page.js
@@ -1,14 +1,15 @@
 import Image from 'next/image'
-
-import Forms from './components/Forms';
+import Forms from './components/Forms'
 import SobreNos from './components/SobreNos'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Starters from './components/Starters'
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Starters/>
       <SobreNos/>
       <Forms/>
       <Footer/>
